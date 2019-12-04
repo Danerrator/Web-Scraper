afd2dd0e1e0ac5cfa26fe81e03844c59eae36a85
Dane Rugen <danerugen@gmail.com>
2019-12-03 21:03:26 -0800

    2

--- server.js
+++ server.js
@@ -5,21 +5,18 @@ var logger = require("morgan");
 var express = require("express");
 var app = express();
 
-app.use(logger("dev"));
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+var db = mongoose.connection;
 
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(process.cwd() + "/public"));
 
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ default: "main"}));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/scraped_news", { useUnifiedTopology:true });
-var db = mongoose.connection;
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function() {
   console.log("Connected to Mongoose!");
